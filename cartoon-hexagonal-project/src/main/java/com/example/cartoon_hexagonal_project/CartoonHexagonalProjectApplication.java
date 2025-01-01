11750fb256138af7bf965caa61c0ddb156aa1303
abhisvpcet <103098612+abhisvpcet@users.noreply.github.com>
2025-01-01 12:11:04 +0530

    Add files via upload

---
+++ cartoon-hexagonal-project/src/main/java/com/example/cartoon_hexagonal_project/CartoonHexagonalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cartoon_hexagonal_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoonHexagonalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartoonHexagonalProjectApplication.class, args);
+	}
+
+}
---
+++ cartoon-hexagonal-project/src/test/java/com/example/cartoon_hexagonal_project/CartoonHexagonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cartoon_hexagonal_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoonHexagonalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
